6b8f27813d6053e62e86bb444e36cf94e4f49025
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-07-02 07:33:54 -0700

    in progress

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,7 +1,11 @@
 class PartiesController < ApplicationController
 
     def index
-        @parties = Party.parties
-        
+        @parties = Party.parties   
     end
+
+    def show
+        @party = Party.find_by_id(params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get '/parties', to: 'parties#index'
+  #get '/parties', to: 'parties#index'
+  resources :parties 
   # For details on the DSL available within this file, see https://guides.rubyonrg/routing.rails.ohtml
 end
