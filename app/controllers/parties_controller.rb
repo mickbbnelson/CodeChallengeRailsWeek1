ae385b7b27992e2cc62c2e3335dc1dab94ac6175
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-07-02 10:57:44 -0400

    new changes

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -8,4 +8,25 @@ class PartiesController < ApplicationController
         @party = Party.find_by_id(params[:id])
     end
 
+    def new
+        @party = Party.new
+    end
+
+    def create
+        @party = Party.new(party_params)
+        @party.save
+        if @party.valid?
+        redirect_to parties_path
+        else
+            render :new
+        end
+
+    end
+
+    private
+
+    def party_params
+    params.require(:party).permit(:name, :date, :category, :supplies, :budget, :private)
+    end
+
 end
